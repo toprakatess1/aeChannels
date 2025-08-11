1c238980ac09a694b181aa6d9758129de8ae7d46
Toprak Ates <toprakateskir31@gmail.com>
2025-08-11 08:09:22 +0300

    aeChannels

---
+++ aechannels/src/main/java/me/toprak/aechannels/AEChannels.java
@@ -0,0 +1,111 @@
+package me.toprak.aechannels;
+
+import com.sk89q.worldedit.util.Location; // WorldEdit'in Location sınıfı
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.*;
+
+public class AEChannels extends JavaPlugin implements Listener {
+
+    private final Map<String, ChatChannel> chatChannels = new HashMap<>();
+    private String localPrefix;
+    private String regionPrefix;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        loadConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("aeChannels enabled!");
+    }
+
+    private void loadConfig() {
+        chatChannels.clear();
+        String gSymbol = getConfig().getString("chatChannels.global.symbol", "!");
+        String gPrefix = getConfig().getString("chatChannels.global.prefix", "[GLOBAL]");
+        chatChannels.put(gSymbol, new ChatChannel(gSymbol, gPrefix));
+
+        String tSymbol = getConfig().getString("chatChannels.trade.symbol", ".");
+        String tPrefix = getConfig().getString("chatChannels.trade.prefix", "[TİCARET]");
+        chatChannels.put(tSymbol, new ChatChannel(tSymbol, tPrefix));
+
+        localPrefix = getConfig().getString("chatChannels.local.prefix", "[LOCAL]");
+        regionPrefix = getConfig().getString("chatChannels.region.prefix", "[REGION]");
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        Player sender = event.getPlayer();
+        String message = event.getMessage();
+
+        Optional<ChatChannel> channelOpt = chatChannels.keySet().stream()
+                .filter(message::startsWith)
+                .map(chatChannels::get)
+                .findFirst();
+
+        event.setCancelled(true);
+
+        if (channelOpt.isPresent()) {
+            ChatChannel channel = channelOpt.get();
+            String msgWithoutSymbol = message.substring(channel.symbol.length()).trim();
+            String formatted = channel.prefix + " " + sender.getName() + ": " + msgWithoutSymbol;
+
+            if (channel.symbol.equals("!")) {
+                Bukkit.getOnlinePlayers().forEach(p -> p.sendMessage(formatted));
+            } else if (channel.symbol.equals(".")) {
+                sender.getWorld().getPlayers().forEach(p -> p.sendMessage(formatted));
+            }
+        } else {
+            WorldGuard wg = WorldGuard.getInstance();
+            RegionContainer regionContainer = wg.getPlatform().getRegionContainer();
+            RegionQuery query = regionContainer.createQuery();
+
+            Location wgLocation = BukkitAdapter.adapt(sender.getLocation());
+            ApplicableRegionSet regions = query.getApplicableRegions(wgLocation);
+
+            if (regions != null && !regions.getRegions().isEmpty()) {
+                String formatted = regionPrefix + " " + sender.getName() + ": " + message;
+                Set<Player> recipients = new HashSet<>();
+
+                for (ProtectedRegion region : regions) {
+                    for (Player p : Bukkit.getOnlinePlayers()) {
+                        Location playerLoc = BukkitAdapter.adapt(p.getLocation());
+                        if (region.contains(playerLoc.getBlockX(), playerLoc.getBlockY(), playerLoc.getBlockZ())) {
+                            recipients.add(p);
+                        }
+                    }
+                }
+
+                if (!recipients.isEmpty()) {
+                    recipients.forEach(p -> p.sendMessage(formatted));
+                    return;
+                }
+            }
+
+            // Bölge yoksa local chat
+            String formatted = localPrefix + " " + sender.getName() + ": " + message;
+            sender.getWorld().getPlayers().forEach(p -> p.sendMessage(formatted));
+        }
+    }
+
+    private static class ChatChannel {
+        String symbol;
+        String prefix;
+
+        ChatChannel(String symbol, String prefix) {
+            this.symbol = symbol;
+            this.prefix = prefix;
+        }
+    }
+}
